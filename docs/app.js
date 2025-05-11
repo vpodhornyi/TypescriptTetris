bc9d7b0ff2b6ea2be99be379b528303251e8d60b
Viktor <vpodhornyi@gmail.com>
2025-05-11 20:17:47 +0200

    delete directory docs from main

--- docs/app.js
+++
@@ -1,7 +0,0 @@
-import { Game } from "./Game.js";
-import { UIManager } from "./UIManager.js";
-import { defaultConfig as config } from "./config/defaultConfig.js";
-const ui = new UIManager(config);
-const game = new Game(ui, config);
-game.init(config);
-ui.startGame(game, config);
