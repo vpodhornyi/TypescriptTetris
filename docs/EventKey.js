f547d7f51a51333d88c88d4cb2e1e67d7442a15e
Viktor <vpodhornyi@gmail.com>
2025-05-11 19:54:19 +0200

    copy dist to docs

--- docs/EventKey.js
+++
@@ -1,7 +0,0 @@
-export var EventKey;
-(function (EventKey) {
-    EventKey["UP"] = "ArrowUp";
-    EventKey["LEFT"] = "ArrowLeft";
-    EventKey["RIGHT"] = "ArrowRight";
-    EventKey["DOWN"] = "ArrowDown";
-})(EventKey || (EventKey = {}));
--- docs/EventKeyEnum.js
+++
@@ -1,8 +0,0 @@
-export var EventKeyEnum;
-(function (EventKeyEnum) {
-    EventKeyEnum["KEYDOWN"] = "keydown";
-    EventKeyEnum["UP"] = "ArrowUp";
-    EventKeyEnum["LEFT"] = "ArrowLeft";
-    EventKeyEnum["RIGHT"] = "ArrowRight";
-    EventKeyEnum["DOWN"] = "ArrowDown";
-})(EventKeyEnum || (EventKeyEnum = {}));
